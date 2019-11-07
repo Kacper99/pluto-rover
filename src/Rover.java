ea609757c3ff7d2032cc9b9da476d8d489c7a0f0
Kacper <kacper99@hotmail.co.uk>
2019-11-07 22:15:28 +0000

    Fix negative mod and implement tests

--- src/Rover.java
+++ src/Rover.java
@@ -8,15 +8,44 @@ public class Rover {
         NORTH, EAST, SOUTH, WEST
     }
 
-    int x, y;
-    int sizeX, sizeY; //Size of the map
-    int direction; //0 = North, 1 = East, 2 = South, 3 = West
-    Map<Integer, ArrayList<Integer>> obstalces = new HashMap<>(); //Represent obstacle positions. i.e. [0, [1,2,3]] represents all the obstacles at x = 0;
+    private int x, y; //Rover position
+    private int sizeX, sizeY; //Size of the map
+    private int direction; //0 = North, 1 = East, 2 = South, 3 = West
+    private Map<Integer, ArrayList<Integer>> obstalces = new HashMap<>(); //Represent obstacle positions. i.e. [0, [1,2,3]] represents all the obstacles at x = 0;
 
     public Rover() {
         this.x = this.y = 0;
         this.direction = 0;
         this.sizeX = this.sizeY = 100;
+    }
+
+    public Rover(int sizeX, int sizeY, int x, int y) {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void printPosition() {
+        String directionName = "";
+        switch (direction) {
+            case 0:
+                directionName = "North";
+                break;
+
+            case 1:
+                directionName = "East";
+                break;
+
+            case 2:
+                directionName = "South";
+                break;
+
+            case 3:
+                directionName = "West";
+                break;
+        }
+        System.out.println("(" + x + ", " + y + ") - " + directionName);
     }
 
     public void move(String instructions) {
@@ -25,34 +54,46 @@ public class Rover {
             switch (m) {
                 case 'F':
                     if (direction == 0)
-                        y = (y + 1) % this.sizeY;
+                        y = Math.floorMod(y + 1, this.sizeY);
                     if (direction == 1)
-                        x = (x + 1) % this.sizeX;
+                        x = Math.floorMod(x + 1, this.sizeX);
                     if (direction == 2)
-                        y = (y - 1) % this.sizeY;
+                        y = Math.floorMod(y - 1, this.sizeY);
                     if (direction == 3)
-                        x = (x - 1) & this.sizeX;
+                        x = Math.floorMod(x - 1, this.sizeX);
                     break;
 
                 case 'B':
                     if (direction == 0)
-                        y = (y - 1) % this.sizeY;
+                        y = Math.floorMod(y - 1, this.sizeY);
                     if (direction == 1)
-                        x = (x - 1) % this.sizeX;
+                        x = Math.floorMod(x - 1, this.sizeX);
                     if (direction == 2)
-                        y = (y + 1) % this.sizeY;
+                        y = Math.floorMod(y + 1, this.sizeY);
                     if (direction == 3)
-                        x = (x + 1) & this.sizeX;
+                        x = Math.floorMod(x + 1, this.sizeX);
                     break;
 
                 case 'L':
-                    direction = (direction - 1) % 4;
+                    direction = Math.floorMod(direction - 1, 4);
                     break;
 
                 case 'R':
-                    direction = (direction + 1) % 4;
+                    direction = Math.floorMod(direction + 1, 4);
                     break;
             }
         }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getDirection() {
+        return direction;
     }
 }
--- src/RoverTest.java
+++ src/RoverTest.java
@@ -1,5 +1,53 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoverTest {
 
+    Rover rover;
+
+    @BeforeEach
+    void before() {
+        rover = new Rover();
+    }
+
+    @Test
+    void simpleMoves() {
+        String path = "FFFRFFFLFFFRBBB"; //(6, 0)
+        rover.move(path);
+        assertEquals(0, rover.getX());
+        assertEquals(6, rover.getY());
+        assertEquals(1, rover.getDirection());
+    }
+
+    @Test
+    void testNorthWrap() {
+        rover = new Rover(10, 10, 0, 9);
+        String path = "F";
+        rover.move(path);
+        assertEquals(0, rover.getY());
+    }
+
+    @Test
+    void testSouthWrap() {
+        rover = new Rover(10, 10, 0, 0);
+        rover.move("B");
+        assertEquals(9, rover.getY());
+    }
+
+    @Test
+    void testEastWrap() {
+        rover = new Rover(10, 10, 9, 0);
+        rover.move("RF");
+        assertEquals(0, rover.getX());
+    }
+
+    @Test
+    void testWestWrap() {
+        rover = new Rover(10, 10, 0, 0);
+        rover.move("LF");
+        rover.printPosition();
+        assertEquals(9, rover.getX());
+    }
 }
